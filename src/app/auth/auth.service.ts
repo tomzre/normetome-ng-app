8b44c6a9ed5115fd94b3d6875b0029c720735c75
System Administrator <root@Tomzs-MacBook-Air.local>
2017-12-21 19:40:44 +0100

    Update auth0-js

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -64,12 +64,6 @@ export class AuthService {
       decodedToken = jwtHelper.decodeToken(authResult);
       this.roles = decodedToken['https://tobenorme.com/roles'];
     }
-<<<<<<< HEAD
-=======
-     
-   
-    console.log('gettingRoles');
->>>>>>> ab6376c03be7a5b8a4bd721bfb54026013f5755f
   }
 
   private setSession(authResult): void {
